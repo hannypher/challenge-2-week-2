2cab68612bc07a8565838020966ed217cb71b036
hannypher <hannypherneenah@gmail.com>
2019-02-15 21:01:42 +0300

    created a commit

---
+++ lists.py
@@ -0,0 +1,7 @@
+string=("g, h, i, j, k, e, d, f, a, r:")
+vowels=0
+for c in string:
+      if(c=='a' or c=='e' or c=='i' or c=='o' or c=='u' or c=='A' or c=='E' or c=='I' or c=='O' or  c=='U'):
+            vowels=vowels+1
+print("Number of vowels are:")
+print(vowels)
