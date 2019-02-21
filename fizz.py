4d33eaec0a69751da1766457be109f57c7060769
hannypher <hannypherneenah@gmail.com>
2019-02-21 22:05:36 +0300

    modified on fizzbuzz file

--- fizz.py
+++ fizz.py
@@ -1,7 +1,7 @@
-def fizzbuzz(h, n):
-    h=len(2,4,6,8,10)
-    n=len(1,3,5,7,9)
-    w=len(h)+len(n)
+def fizzbuzz(lista, listb):
+    h = len(lista)
+    n = len(listb)
+    w = h + n
     if w % 3 == 0:
         return "fizz"
     elif w % 5 == 0:
